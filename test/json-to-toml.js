6e83d1a9092354a0ee2770b525d812a85aac72dc
Forrest L Norvell <forrest@npmjs.com>
2015-03-15 23:46:32 -0700

    test: real test of JSON -> TOML

--- test/json-to-toml.js
+++ test/json-to-toml.js
@@ -1,16 +1,30 @@
 var concat = require('concat-stream')
 var test = require('tap').test
+var toml = require('toml')
 var JSONStream = require('JSONStream')
+var TOMLStream = require('../')
 
 test('emitting streaming JSON', function (t) {
-  var stream = JSONStream.stringify()
-  stream.pipe(concat(function (output) {
-    t.equals(output, '[\n{"number":314}\n]\n', 'got expected output')
-    t.end()
-  }))
-  stream.on('error', function (er) {
-    t.ifError(er, "shouldn't have failed to write a stream this simple")
-  })
+  var input = {
+    name: 'sample',
+    values: [
+      {number1: 314},
+      {number2: 271}
+    ]
+  }
+  var stream = JSONStream.parse('values.*')
 
-  stream.end({number: 314})
+  stream.pipe(new TOMLStream())
+        .pipe(concat(function (output) {
+          t.equals(output, 'number1 = 314\nnumber2 = 271\n')
+          t.same(toml.parse(output), {number1: 314, number2: 271}, 'roundtrip succeeded')
+          t.end()
+        }))
+        .on('error', function (er) {
+          t.ifError(er, "shouldn't have failed to transform a simple stream")
+          t.fail("couldn't transform simple stream")
+          t.end()
+        })
+
+  stream.end(JSON.stringify(input))
 })
