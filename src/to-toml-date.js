f88a8c4b3e1de455f233e4537fa3e7f43405db17
Forrest L Norvell <forrest@npmjs.com>
2015-03-20 22:40:28 -0700

    dates: eliminated need for moment
    Thanks, `toml` maintainers!

--- src/to-toml-date.js
+++ src/to-toml-date.js
@@ -1,6 +1,4 @@
-import moment from 'moment'
-
 export default function toTOMLDate (date) {
   // Is this always RFC3339-compliant? I DON'T KNOW
-  return moment(date).format('YYYY-MM-DDTHH:mm:ss.SSSZ')
+  return date.toISOString()
 }
