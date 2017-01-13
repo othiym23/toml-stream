11446e466e787b723591684dacef558c5cc43fba
Forrest L Norvell <forrest@npmjs.com>
2017-01-12 17:08:49 -0800

    src: minor simplification

--- src/codecs/object.js
+++ src/codecs/object.js
@@ -4,12 +4,25 @@ import { encode, getTypeTag } from './index.js'
 export default class ObjectTOMLCodec extends TOMLCodec {
   // override the standard behavior because of our need for state
   encode (key, value, writable, path = []) {
-    const { values, objects } = partition(value)
+    const values = []
+    const objects = []
 
+    // non-object plain values need to go first
+    Object.keys(value).forEach(key => {
+      if (getTypeTag(value[key]) === 'object') {
+        objects.push(key)
+      } else {
+        values.push(key)
+      }
+    })
+
+    // only emit an ["object key"] if there are actual variables to capture
     if (values.length) {
       if (writable.started) writable.push('\n')
 
       let name = '[' + path.map(p => this._encodeKey(p)).join('.') + ']'
+      // FIXME: this is probably going to break things; is a cheesy hack to
+      // indicate tabular array elements
       if (!key) name = '[' + name + ']'
       writable.push(name + '\n')
     }
@@ -17,18 +30,4 @@ export default class ObjectTOMLCodec extends TOMLCodec {
     values.forEach(k => encode(k, value, writable, path))
     objects.forEach(k => encode(k, value, writable, path))
   }
-}
-
-function partition (object) {
-  const values = []
-  const objects = []
-  for (let key of Object.keys(object)) {
-    if (getTypeTag(object[key]) === 'object') {
-      objects.push(key)
-    } else {
-      values.push(key)
-    }
-  }
-
-  return { values, objects }
 }
